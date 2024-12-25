b5c042dfc7cc032e87ea98cf7d64b0d10d90263d
hasanjon <hasanjon1224@gmail.com>
2024-12-25 10:09:15 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React, { Component } from "react"
-import Input from "./components/Input"
-// import Foo from './components/User';
-// import { Baz, Bar } from "./components/User"
+import React from 'react'
+import Input from './components/Todos'
+
+const App = () => {
+    return <>
+        <Input />
+    </>
 
 
 
-export default class App extends Component {
-    render() {
-        return <>
-            {/* <Users name={Hasanxon} />
-            <Users lastName={Abdulazizov} />
-            <Users age={21} />
-            <Users job={Programming} />
-            <Users />
-            <UseState /> */}
-            {/* <Foo name="hasanxon" />
-            <Foo name={"Jasur"}>
-                Informatsiya
-            </Foo> */}
-            {/* <Baz />
-            <Bar /> */}
-            <Input />
-        </>
-    }
 
 }
+
+export default App;
